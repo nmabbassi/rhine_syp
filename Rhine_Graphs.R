2bd498101cbf7909f3e1f6f303affda37c315a9e
Nakiessa Abbassi <nabbassi@uoregon.edu>
2021-01-18 13:57:12 -0800

    someone give me a cs degree i figured out the legend

--- Rhine_Graphs.R
+++ Rhine_Graphs.R
@@ -7,57 +7,86 @@ library(readxl)
 rh_data <- read_excel("~/Desktop/Rhine/Rhine_Chloride_Total.xlsx",
                       na="NA") 
 
+
+
+
 #fig. 1 - average chloride pollution in Rhine
+  #code ggplot
 ggplot(rh_data, aes(x=`Year`, 
-                    y=`Total Averages`))+
+                    y=`Total Averages`,
+                    color="Total Averages"))+
   geom_line()+
   labs(x=element_blank(), 
        y="Average Chloride Ion Pollution in Rhine River (kg/s)", 
        title=element_blank(),
        fill=element_blank())+
   theme_gray()+      
+  
   #add vert lines for treaty introduction
   geom_vline(aes(xintercept=1976,
                  color="Treaty Signed"),
              linetype="longdash",
-             size=1)+
+             size=.5)+
   geom_vline(aes(xintercept=1986,
                  color="Treaty Ratified"),
              linetype="dashed",
-             size=1)+
-  scale_color_manual(name=element_blank(), 
+             size=.5)+
+  #create legend
+   scale_color_manual(name=element_blank(), 
                      values = c(`Treaty Signed` = "gray7", 
-                                `Treaty Ratified` = "gray27"))+
+                                `Treaty Ratified` = "gray27",
+                                `Total Averages`= "steelblue4"))+
+  
       theme(legend.position="bottom", legend.background=element_rect
            (fill="gray95", linetype=1, size=0.15, color=1))
 
-#fig.2 - avg. chloride pollution in rhine per state
-  #create combined country data frame
-labels <- c(`Switzerland` = "red4", 
-           `Netherlands` = "dodgerblue4", 
-           `Germany` = "slateblue",
-           `France` = "darkgreen")
 
+
+#fig.2 - avg. chloride pollution in rhine per state
+  #create data frame for country info
+    labels <- c(`Switzerland` = "red4", 
+                `Netherlands` = "dodgerblue4", 
+                `Germany` = "slateblue",
+                `France` = "darkgreen")
+  #create data frame for horizontal lines to later add to legend
+    treatysig <- c(xintercent= 1976)
+    treatyrat <- c(xintercept=1986)
+
+    dftest <- c(treatysig = "gray7",
+                treatyrat = "gray27")
+
+  #code ggplot
 ggplot(rh_data, aes(x=`Year`))+
-          geom_line(aes(y=`Switzerland`, color="Switzerland"))+
-          geom_line(aes(y=`Netherlands`, color="Netherlands"))+
-          geom_line(aes(y=`Germany`, color="Germany"))+
-          geom_line(aes(y=`France`, color="France"))+
-    labs(x=element_blank(), 
-         y="Average Chloride Ion Pollution in Rhine River (kg/s) per State", 
-         title=element_blank(),
-         fill=element_blank(),
-         color=element_blank())+
-      scale_color_manual(values=labels)+
+  geom_line(aes(y=`Switzerland`, 
+                color="Switzerland"))+
+  geom_line(aes(y=`Netherlands`, 
+                color="Netherlands"))+
+  geom_line(aes(y=`Germany`, 
+                color="Germany"))+
+  geom_line(aes(y=`France`, 
+                color="France"))+
+  
+  labs(x=element_blank(), 
+       y="Average Chloride Ion Pollution in Rhine River (kg/s) per State", 
+       title=element_blank(),
+       fill=element_blank(),
+       color=element_blank())+
+ 
+  #add horizontal lines for treaty info
+  geom_vline(aes(xintercept=treatysig,
+             color="Treaty Signed"),
+             linetype = "longdash",
+             size=.5)+
+  geom_vline(aes(xintercept = treatyrat,
+             color="Treaty Ratified"),
+             linetype = "dashed",
+             size=.5)+
+  
+  #create legend
+  scale_color_manual(name=element_blank(),
+                     values=c(labels, 
+                              `Treaty Signed` = "gray7",
+                              `Treaty Ratified` = "gray27"))+
   theme_gray()+
   theme(legend.position="bottom", legend.background=element_rect
-        (fill="gray95", linetype=1, size=0.15, color=1))+
- 
-   geom_vline(xintercept = 1976, 
-             linetype="longdash",
-             color="gray7",
-             size=1)+
-   geom_vline(xintercept = 1988, 
-             linetype="dashed",
-             color="gray27",
-             size=1)
+        (fill="gray95", linetype=1, size=.15, color=1))
