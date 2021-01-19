35efc1bf15e0f70e644005d8cac67a8511c16d64
Nakiessa Abbassi <nabbassi@uoregon.edu>
2021-01-18 18:23:44 -0800

    fig. 3

--- Rhine_Graphs.R
+++ Rhine_Graphs.R
@@ -10,7 +10,7 @@ rh_data <- read_excel("~/Desktop/Rhine/Rhine_Chloride_Total.xlsx",
 
 
 
-#fig. 1 - average chloride pollution in Rhine
+#fig.1 - avg. chloride pollution in Rhine
   #code ggplot
 ggplot(rh_data, aes(x=`Year`, 
                     y=`Total Averages`,
@@ -90,3 +90,51 @@ ggplot(rh_data, aes(x=`Year`))+
   theme_gray()+
   theme(legend.position="bottom", legend.background=element_rect
         (fill="gray95", linetype=1, size=.15, color=1))
+
+
+#fig.3 - avg. chloride concentration in Rhine
+  #note flow rate data is limited - both in years and from monitoring stations
+    #calculating concentrations (kg/s) divided by (m3/s) = kg/m3
+rh_data$swiss_conc <- 100*(rh_data$Switzerland/rh_data$FR_Switzerland)
+rh_data$neth_conc <- 100*(rh_data$Netherlands/rh_data$FR_Netherlands)
+rh_data$germ_conc <- 100*(rh_data$Germany/rh_data$FR_Germany)
+rh_data$france_conc <- 100*(rh_data$France/rh_data$FR_France)
+
+  #code ggplot
+ggplot(rh_data, aes(x=`Year`))+
+  geom_line(aes(y=swiss_conc, 
+                color="Switzerland"))+
+  geom_line(aes(y=neth_conc, 
+                color="Netherlands"))+
+  geom_line(aes(y=germ_conc, 
+                color="Germany"))+
+  geom_line(aes(y=france_conc, 
+                color="France"))+
+
+  labs(x=element_blank(), 
+       y="Average Chloride Concentrations in Rhine River (kg/m3) per State", 
+       title=element_blank(),
+       fill=element_blank(),
+       color=element_blank())+
+
+  #add horizontal lines for treaty info
+  geom_vline(aes(xintercept=treatysig,
+                 color="Treaty Signed"),
+                 linetype = "longdash",
+                 size=.5)+
+  geom_vline(aes(xintercept = treatyrat,
+                 color="Treaty Ratified"),
+                 linetype = "dashed",
+                 size=.5)+
+  #code legend
+    scale_color_manual(name=element_blank(),
+                       values=c(labels, 
+                                `Treaty Signed` = "gray7",
+                                `Treaty Ratified` = "gray27"))+
+    theme_gray()+
+    theme(legend.position="bottom", legend.background=element_rect
+         (fill="gray95", linetype=1, size=.15, color=1))
+
+#fig. 3 - avg. chloride pollution at selected monitoring stations
+
+#fig. 4 - avg. chloride concentrations
