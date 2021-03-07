aaaea858282b9f0b14df0b1adb5cbb48e2f761b9
Nakiessa Abbassi <nabbassi@uoregon.edu>
2021-03-07 00:24:41 -0800

    rhine flow rate graph

--- Rhine_Graphs.R
+++ Rhine_Graphs.R
@@ -1,15 +1,56 @@
 install.packages("tidyverse")
 install.packages("readxl")
 install.packages("ca")
+install.packages("forecast")
+install.packages("MLmetrics")
 library(ggplot2)
 library(readxl)
 library(carData)
+
+#below used for forecasting counterfactual
+library(forecast)
+library(MLmetrics)
 
 #load data
 rh_data <- read_excel("~/Desktop/Rhine/Rhine_Chloride_Total.xlsx",
                       na="NA") 
 
 ad <- read_excel("argumentdata.xlsx")
+
+rh_forecast <- read_excel("~/Desktop/Rhine/rhine_forecast.xlsx",
+                          na="NA") 
+
+rh_avgflow <- read_excel("~/Desktop/Rhine/rhine_avgflow.xlsx",
+                         na="NA")
+
+#fig. 1: total average flow rate
+  #create lines and labels for each country
+labels <- c(`Switzerland` = "red4", 
+            `Netherlands` = "dodgerblue4", 
+            `Germany` = "slateblue",
+            `France` = "darkgreen")
+  #code ggplot
+ggplot(rh_avgflow, aes(x=`Year`))+
+  geom_line(aes(y=`Switzerland`, 
+                color="Switzerland"))+
+  geom_line(aes(y=`Netherlands`, 
+                color="Netherlands"))+
+  geom_line(aes(y=`Germany`, 
+                color="Germany"))+
+  geom_line(aes(y=`France`, 
+                color="France"))+
+  labs(x=element_blank(), 
+       y="Average Annual Flow Rate 
+in Rhine River (m3/s)", 
+       title=element_blank(),
+       fill=element_blank())+
+  theme_gray()+ 
+  theme(axis.title=element_text(size=12))+
+  scale_color_manual(name=element_blank(),
+                     values=c(labels))+
+  theme(legend.position="bottom", legend.background=element_rect
+        (fill="gray95", linetype=1, size=.15, color=1))
+
 
 #fig. x argument simple scatterplot
 plot(ad, 
@@ -19,6 +60,7 @@ plot(ad,
 
 #fig.1 - avg. chloride pollution in Rhine
   #code ggplot
+
 ggplot(rh_data, aes(x=`Year`, 
                     y=`Total Averages`,
                     color="Total Averages"))+
@@ -151,4 +193,5 @@ in Rhine River (kg/m3) per State",
 #fig. 4 - avg. chloride pollution at selected monitoring stations
 
 
+#fig. i orignal forecasting test exp model
 
