a001c4fd9e27d037037b1c5113a15ef97efebf7d
Nakiessa Abbassi <nabbassi@uoregon.edu>
2021-03-24 00:48:42 -0700

    edit ggplot

--- rhine_counterfactual.R
+++ rhine_counterfactual.R
@@ -7,7 +7,11 @@ library(carData)
 ##Potash Demand
 ##flow rate
 ##growing environmentalism in region
+treatysig <- c(xintercent = 1976)
+treatyrat <- c(xintercept = 1986)
 
+dftest <- c(treatysig = "gray7",
+            treatyrat = "gray27")
 
 ##### Potash Consumption #####
 ###fertilizer consumption measures quantity of plant nutrients
@@ -18,7 +22,7 @@ fert_consump <- read_excel("~/Desktop/Rhine/FR_fert_consumption.xlsx",
 #code ggplot
 ggplot(fert_consump, aes(x=`Year`, 
                          y=`Fertilizer`),
-                    color="Fertilizer Consumption")+
+                         color="Fertilizer Consumption")+
   geom_line()+
   labs(x=element_blank(), 
        y="Fertilizer Consumption", 
@@ -28,21 +32,23 @@ ggplot(fert_consump, aes(x=`Year`,
   theme(axis.title=element_text(size=12))+
   
   #add vert lines for treaty introduction
-  geom_vline(aes(xintercept=1976,
-                 color="Treaty Signed"),
-             linetype="longdash",
-             size=.5)+
-  geom_vline(aes(xintercept=1986,
-                 color="Treaty Ratified"),
-             linetype="dashed",
-             size=.5)
+      geom_vline(aes(xintercept = treatysig,
+                     color="Treaty Signed"),
+                     linetype = "longdash",
+                     size=.5)+
+      geom_vline(aes(xintercept = treatyrat,
+                     color="Treaty Ratified"),
+                     linetype = "dashed",
+                     size=.5)+
   #create legend
-######
-  #scale_color_manual(name=element_blank(), 
-                #     values = c(`Treaty Signed` = "gray7", 
-                 #               `Treaty Ratified` = "gray27",
-                 #               `Chloride Ion Averages (kg/s)`= "steelblue4",
-                  #              `Chloride Concentration (kg/m3)`= "blue3"))+
+      scale_color_manual(name=element_blank(),
+                     values=c(labels, 
+                              `Treaty Signed` = "gray7",
+                              `Treaty Ratified` = "gray27"))+
+      theme_gray()+
+      theme(axis.title=element_text(size=12))+
+      theme(legend.position="bottom", legend.background=element_rect
+           (fill="gray95", linetype=1, size=.15, color=1))
   
-#  theme(legend.position="bottom", legend.background=element_rect
-    #    (fill="gray95", linetype=1, size=0.15, color=1))
+
+
