d7108ccb046316fc0c915ac23b19f2bce684f9d5
Nakiessa Abbassi <nabbassi@uoregon.edu>
2021-03-24 21:15:13 -0700

    gettin there

--- rhine_counterfactual.R
+++ rhine_counterfactual.R
@@ -7,8 +7,10 @@ library(carData)
 ##Potash Demand
 ##flow rate
 ##growing environmentalism in region
-treatysig <- c(xintercent = 1976)
-treatyrat <- c(xintercept = 1986)
+treatyrat <- c(xintercept = 1983)
+treatyimp <- c(xintercept = 1986)
+protocolrat <- c(xintercept = 1991)
+protocolimp <- c(xintercept = 1994)
 
 dftest <- c(treatysig = "gray7",
             treatyrat = "gray27")
@@ -21,34 +23,41 @@ fert_consump <- read_excel("~/Desktop/Rhine/FR_fert_consumption.xlsx",
 
 #code ggplot
 ggplot(fert_consump, aes(x=`Year`, 
-                         y=`Fertilizer`),
-                         color="Fertilizer Consumption")+
+                         y=`Fertilizer`,
+                         color="    Fertilizer Consumption"))+
   geom_line()+
   labs(x=element_blank(), 
-       y="Fertilizer Consumption", 
+       y="Fertilizer Consumption (% of fertilizer production),
+includes nitrogen, potash, and phosphate fertlizers", 
        title=element_blank(),
        fill=element_blank())+
   theme_gray()+ 
   theme(axis.title=element_text(size=12))+
   
   #add vert lines for treaty introduction
-      geom_vline(aes(xintercept = treatysig,
-                     color="Treaty Signed"),
-                     linetype = "longdash",
-                     size=.5)+
-      geom_vline(aes(xintercept = treatyrat,
-                     color="Treaty Ratified"),
-                     linetype = "dashed",
-                     size=.5)+
+  geom_vline(aes(xintercept=treatyrat,
+                 color="   Treaty Ratified"),
+             linetype = "solid",
+             size=.5)+
+  geom_vline(aes(xintercept = treatyimp,
+                 color="  Treaty Implemented"),
+             linetype = "longdash",
+             size=.5)+
+  geom_vline(aes(xintercept = protocolrat,
+                 color=" Protocol Ratified"),
+             linetype = "dotdash",
+             size=.5)+
+  geom_vline(aes(xintercept = protocolimp,
+                 color="Protocol Implemented"),
+             linetype = "twodash",
+             size=.5)+
   #create legend
       scale_color_manual(name=element_blank(),
-                     values=c(labels, 
-                              `Treaty Signed` = "gray7",
-                              `Treaty Ratified` = "gray27"))+
-      theme_gray()+
-      theme(axis.title=element_text(size=12))+
-      theme(legend.position="bottom", legend.background=element_rect
-           (fill="gray95", linetype=1, size=.15, color=1))
-  
-
+                     values=c( `   Treaty Ratified` = "gray1",
+                               `  Treaty Implemented` = "gray1",
+                               ` Protocol Ratified` = "gray30",
+                               `Protocol Implemented` = "gray30",
+                              `    Fertilizer Consumption` = "blue4"))+
+  theme(legend.position="bottom", legend.background=element_rect
+        (fill="gray95", linetype=1, size=.15, color=1))
 
